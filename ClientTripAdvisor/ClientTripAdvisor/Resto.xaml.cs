f03627b809b5de01e80fbb426c0a281c9917c7b2
Mi2pain <hug93@hotmail.fr>
2016-10-12 09:29:59 +0200

    Modify

--- ClientTripAdvisor/ClientTripAdvisor/Resto.xaml.cs
+++ ClientTripAdvisor/ClientTripAdvisor/Resto.xaml.cs
@@ -79,6 +79,7 @@ namespace ClientTripAdvisor
             this.dataGrid.DataContext = dt.DefaultView;
             this.Id_ComboBox.DataContext = dt.DefaultView;
             this.Del_Combobox.DataContext = dt.DefaultView;
+            this.Id_ComboBox_mod.DataContext = dt.DefaultView;
         }
 
         private void Id_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -224,6 +225,134 @@ namespace ClientTripAdvisor
 
                     // Do the actual request and await the response
                     var httpResponse = await httpClient.DeleteAsync(urlAPI + "Restaurant/" + Del_Combobox.Text);
+                }
+            }
+            refresh_data();
+        }
+
+        private void Afficher_modifier_Click(object sender, RoutedEventArgs e)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAPI + "Restaurant/" + Id_ComboBox_mod.Text);
+
+            request.Accept = "application/json";
+
+            // Set some reasonable limits on resources used by this request 
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            // Set credentials to use for this request. 
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            // Get the stream associated with the response. 
+            Stream receiveStream = response.GetResponseStream();
+
+            // Pipes the stream to a higher level stream reader with the required encoding format.  
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+
+            Restaurant reponse = JsonConvert.DeserializeObject<Restaurant>(readStream.ReadToEnd());
+
+            if (reponse.PRX_PRIX != null)
+            {
+                comboBox1_mod.Text = reponse.PRX_PRIX;
+            }
+
+            if (reponse.RES_NOM != null)
+            {
+                Nom_TextBox_mod.Clear();
+                Nom_TextBox_mod.AppendText(reponse.RES_NOM);
+            }
+
+            if (reponse.RES_CATEGORIEPRIX != null)
+            {
+                Cat_TextBox_mod.Clear();
+                Cat_TextBox_mod.AppendText(reponse.RES_CATEGORIEPRIX);
+            }
+
+            if (reponse.RES_DESCRIPTION != null)
+            {
+                Description_TextBox_mod.Clear();
+                Description_TextBox_mod.AppendText(reponse.RES_DESCRIPTION);
+            }
+
+            if (reponse.res_adr != null)
+            {
+                Adresse_TextBox_mod.Clear();
+                Adresse_TextBox_mod.AppendText(reponse.res_adr);
+            }
+
+            if (reponse.RES_CP != null)
+            {
+                CP_TextBox_mod.Clear();
+                CP_TextBox_mod.AppendText(reponse.RES_CP);
+            }
+
+            if (reponse.RES_VILLE != null)
+            {
+                Ville_TextBox_mod.Clear();
+                Ville_TextBox_mod.AppendText(reponse.RES_VILLE);
+            }
+
+            if (reponse.RES_PAYS != null)
+            {
+                Pays_TextBox_mod.Clear();
+                Pays_TextBox_mod.AppendText(reponse.RES_PAYS);
+            }
+
+            if (reponse.RES_TEL != null)
+            {
+                Tel_TextBox_mod.Clear();
+                Tel_TextBox_mod.AppendText(reponse.RES_TEL);
+            }
+
+            if (reponse.RES_MEL != null)
+            {
+                Mail_TextBox_mod.Clear();
+                Mail_TextBox_mod.AppendText(reponse.RES_MEL);
+            }
+
+            if (reponse.RES_SITEWEB != null)
+            {
+                Web_TextBox_mod.Clear();
+                Web_TextBox_mod.AppendText(reponse.RES_SITEWEB);
+            }
+
+            readStream.Close();
+        }
+
+        private async void modifier_button_Click(object sender, RoutedEventArgs e)
+        {
+            Restaurant postResto = new Restaurant();
+
+            postResto.RES_ID = int.Parse(Id_ComboBox_mod.Text);
+            postResto.RES_NOM = Nom_TextBox_mod.Text;
+            postResto.RES_MEL = Mail_TextBox_mod.Text;
+            postResto.RES_PAYS = Pays_TextBox_mod.Text;
+            postResto.RES_TEL = Tel_TextBox_mod.Text;
+            postResto.RES_CP = CP_TextBox_mod.Text;
+            postResto.RES_CATEGORIEPRIX = Cat_TextBox_mod.Text;
+            postResto.RES_DESCRIPTION = Description_TextBox_mod.Text;
+            postResto.PRX_PRIX = comboBox1_mod.Text;
+            postResto.RES_VILLE = Ville_TextBox_mod.Text;
+            postResto.RES_SITEWEB = Web_TextBox_mod.Text;
+            postResto.res_adr = Adresse_TextBox_mod.Text;
+
+            var json = JsonConvert.SerializeObject(postResto);
+
+
+            var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+            using (var httpClient = new HttpClient())
+            {
+
+                // Do the actual request and await the response
+                var httpResponse = await httpClient.PutAsync(urlAPI + "Restaurant/" + Id_ComboBox_mod.Text, httpContent);
+
+                // If the response contains content we want to read it!
+                if (httpResponse.Content != null)
+                {
+                    var responseContent = await httpResponse.Content.ReadAsStringAsync();
+
+                    // From here on you could deserialize the ResponseContent back again to a concrete C# type using Json.Net
                 }
             }
             refresh_data();
